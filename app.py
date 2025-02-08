13a82a6845bd64545dd5ef271e1089e5c67af0e1
Canopyflick <tenbergeben@gmail.com>
2025-02-08 02:10:13 +0100

    4o fix

--- app.py
+++ app.py
@@ -104,7 +104,7 @@ with gr.Blocks() as demo:
         with gr.Row():
             model_choice = gr.Dropdown(
                 choices=list(llms.keys()),
-                value="gpt4o",
+                value="gpt-4o",
                 label="Select LLM",
                 interactive=True,
             )
